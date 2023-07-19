bfeb026450d91415e6568d0a7d69063a92e1deec
jj wilson <jjwilsonart@gmail.com>
2023-07-19 11:07:09 +0200

    completed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,13 +3,15 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @answer = params[:answer]
-    if @answer == 'I am going to work'
-      @search = 'Great!'
-    elsif @answer.end_with?('?')
-      @search = 'Silly question, get dressed and go to work!'
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
     else
-      @search = 'I dont care, get dressed and go to work!'
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
